a1e97122d57bbb7066bd1a521e9735703fc5794a
Joanna Kao <joanna.kao@ft.com>
2018-10-25 19:14:17 +0100

    add social colours for US midterms 2018

--- build/g-chartcolour.js
+++ build/g-chartcolour.js
@@ -223,7 +223,10 @@
   var categoricalUsPoliticsMidterms2018 = {
     "republican small area": "#E60008",
     "republican large area": "#FF4C55",
-    democrat: "#0F5499"
+    democrat: "#0F5499",
+    "social republican": "#FF5760",
+    "social democrat small area": "#177EE6",
+    "social democrat large area": "#579DD5",
   };
 
   exports.background = backgrounds;
