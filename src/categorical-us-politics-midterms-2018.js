1f0520a914426dd7bef80b7b31edde4f3738b334
Joanna Kao <joanna.kao@ft.com>
2018-10-26 17:29:47 +0100

    oops. swap social dems small and large areas

--- src/categorical-us-politics-midterms-2018.js
+++ src/categorical-us-politics-midterms-2018.js
@@ -3,6 +3,6 @@ export default {
   "republican large area": "#FF4C55",
   democrat: "#0F5499",
   "social republican": "#FF5760",
-  "social democrat small area": "#177EE6",
-  "social democrat large area": "#579DD5",
+  "social democrat small area": "#579DD5",
+  "social democrat large area": "#177EE6",
 };
