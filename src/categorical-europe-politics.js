4a39f4538f8df952fd1cf5cb8ce10c91fab4961e
Ændrew Rininsland <andrew.rininsland@ft.com>
2019-05-10 15:52:41 +0100

    Updating

--- src/categorical-europe-politics.js
+++ src/categorical-europe-politics.js
@@ -5,26 +5,26 @@
 
 export default {
   epp: "#321079",
-  "s&d": "#f95f5f",
+  sd: "#f95f5f",
   alde: "#ffe033",
   enf: "#58aafc",
-  other: "white",
+  others: "white",
   ecr: "#217a9d",
-  ngl: "#991d00",
-  greens: "#8deb9d",
+  guengl: "#991d00",
+  greensefa: "#8deb9d",
   efdd: "#8bd0d6",
   ni: "#9e958b"
 };
 
 export const text = {
   epp: "#321079",
-  "s&d": "#ff3333",
+  sd: "#ff3333",
   alde: "#9e6e15",
   enf: "#0070e0",
-  other: "rgba(0, 0, 0, 0.6)",
+  others: "rgba(0, 0, 0, 0.6)",
   ecr: "#217a9d",
-  ngl: "#991d00",
-  greens: "#008016",
+  guengl: "#991d00",
+  greensefa: "#008016",
   efdd: "#2a9aa7",
   ni: "rgba(0, 0, 0, 0.8)"
 };
